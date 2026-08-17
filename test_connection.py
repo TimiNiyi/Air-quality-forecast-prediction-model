d956f64af0243a16ad3dc4d3bda12297ef97d322
Pamilerin Olaniyi <timiiniyii@gmail.com>
2026-08-17 12:25:40 +0100

    Initial project setup

---
+++ test_connection.py
@@ -0,0 +1,17 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+uri = os.getenv("MONGODB_URI")
+
+client = MongoClient(uri)
+
+try:
+    client.admin.command("ping")
+    print("MongoDB connection successful!")
+except Exception as e:
+    print("MongoDB connection failed:")
+    print(e)
+    
